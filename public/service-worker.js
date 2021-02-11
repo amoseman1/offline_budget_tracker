a8eeee695d6888cc241a5d137c28a9200cb904c8
Waterbug <amoseman18@gmail.com>
2021-02-10 17:12:38 -0700

    added static files to cache array

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,8 +6,13 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
 
-
-
+    "/",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/index.html",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/styles.css"
 
 ]
 
